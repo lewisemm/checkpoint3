15585779e3f4aad7520bc63ad72e859dedcbc44b
Lewis Kabui <lewis.maina@andela.com>
2016-01-02 21:51:11 +0300

    (feature) Create initial tests for TDD

---
+++ tests/test_base_class.py
@@ -0,0 +1,19 @@
+from django.test import TestCase, Client
+from faker import Factory
+
+import models
+
+class TestBaseClass(TestCase):
+
+	def setUp(self):
+		self.fake = Factory.create()
+		self.client = Client()
+
+	def create_user(self):
+		"""
+		This method registers a new user for use in the tests below.
+		"""
+		self.username = self.fake.user_name()
+		self.password = self.fake.password()
+
+		self.client.post('/user/register', {'username': self.username, 'password': self.password})

62b11a1edd1e3c7cadf77865cbc6046c73840e32
Lewis Kabui <lewis.maina@andela.com>
2016-01-03 12:41:12 +0300

    (feature) Add more tests for TDD

---
+++ tests/test_bucketlist_id.py
@@ -0,0 +1,107 @@
+from test_base_class import TestBaseClass
+
+class TestBucketListID(TestBaseClass):
+
+	def test_successful_get_bucketlist_id(self):
+		"""
+		Tests a successful get attempt of a bucketlist of the specified id.
+		"""
+		# create a user
+		self.create_user()
+		# login user
+		self.client.post('/auth/login', {'username': self.username, 'password': self.password})
+		# create a bucketlist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# the get request to bucketlist id 1
+		response = self.client.get('/bucketlists/1', headers={'username': token})
+
+		self.assertTrue(bucketlist_name in response.content)
+		self.assertEqual(response.status_code, 200)
+
+	def test_unauthenticated_get_bucketlist_id(self):
+		"""
+		Tests an unauthenticated get attempt of a bucketlist of the specified id.
+		"""
+		# create a user
+		self.create_user()
+		# login user
+		self.client.post('/auth/login', {'username': self.username, 'password': self.password})
+		# create a bucketlist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# the get request to bucketlist id 1
+		response = self.client.get('/bucketlists/1')
+
+		self.assertTrue('Unauthorised access' in response.content)
+		self.assertEqual(response.status_code, 403)
+
+	def test_successful_put_bucketlist_id(self):
+		"""
+		Tests a successful put attempt of a bucketlist of the specified id.
+		"""
+		# create a user
+		self.create_user()
+		# login user
+		self.client.post('/auth/login', {'username': self.username, 'password': self.password})
+		# create a bucketlist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# the put request to bucketlist id 1
+		new_bucketlist_name = self.fake.name()
+		response = self.client.get('/bucketlists/1', data={'name': new_bucketlist_name}, headers={'username': token})
+
+		self.assertTrue(new_bucketlist_name in response.content)
+		self.assertEqual(response.status_code, 200)
+
+	def test_unauthenticated_put_bucketlist_id(self):
+		"""
+		Tests an unauthenticated put attempt of a bucketlist of the specified id.
+		"""
+		# create a user
+		self.create_user()
+		# login user
+		self.client.post('/auth/login', {'username': self.username, 'password': self.password})
+		# create a bucketlist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# the put request to bucketlist id 1
+		new_bucketlist_name = self.fake.name()
+		response = self.client.get('/bucketlists/1', data={'name': new_bucketlist_name})
+
+		self.assertTrue('Unauthorised access' in response.content)
+		self.assertEqual(response.status_code, 403)
+
+	def test_successful_delete_bucketlist_id(self):
+		"""
+		Tests a successful delete attempt of a bucketlist of the specified id.
+		"""
+		# create a user
+		self.create_user()
+		# login user
+		self.client.post('/auth/login', {'username': self.username, 'password': self.password})
+		# create a bucketlist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# the delete request to bucketlist id 1
+		response = self.client.delete('/bucketlists/1', headers={'username': token})
+
+		self.assertTrue('Bucketlist of id 1 has been deleted' in response.content)
+		self.assertEqual(response.status_code, 200)
+
+	def test_unauthenticated_delete_bucketlist_id(self):
+		"""
+		Tests a successful delete attempt of a bucketlist of the specified id.
+		"""
+		# create a user
+		self.create_user()
+		# login user
+		self.client.post('/auth/login', {'username': self.username, 'password': self.password})
+		# create a bucketlist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# the delete request to bucketlist id 1
+		response = self.client.delete('/bucketlists/1')
+
+		self.assertTrue('Unauthorised access' in response.content)
+		self.assertEqual(response.status_code, 403)
---
+++ tests/test_bucketlist_items_id.py
@@ -0,0 +1,95 @@
+from test_base_class import TestBaseClass
+
+class TestBucketListItemsId(TestBaseClass):
+
+	def test_successful_put_bucketlist_items(self):
+		"""
+		Test successful put attempt on an item in a bucketlist.
+		"""
+		# create the user
+		self.create_user()
+		# login the user
+		self.client.post('/auth/login', data={'username': self.username, 'password': self.password})
+		# create a buckelist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# create an item in the bucketlist
+		item_name = self.fake.name()
+		self.client.post('/bucketlists/1', data={'name': item_name}, headers={'username': token})
+		# update the item
+		new_item_name = self.fake.name()
+		self.client.post('/bucketlists/1/items/1', data={'name': new_item_name}, headers={'username': token})
+
+		self.assertTrue(new_item_name in response.content)
+		self.assertFalse(item_name in response.content)
+		self.assertEqual(response.status_code, 200)
+
+	def test_unauthenticated_put_bucketlist_items(self):
+		"""
+		Test unauthenticated put attempt on an item in a bucketlist.
+		"""
+		# create the user
+		self.create_user()
+		# login the user
+		self.client.post('/auth/login', data={'username': self.username, 'password': self.password})
+		# create a buckelist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# create an item in the bucketlist
+		item_name = self.fake.name()
+		self.client.post('/bucketlists/1', data={'name': item_name}, headers={'username': token})
+		# update the item
+		new_item_name = self.fake.name()
+		self.client.post('/bucketlists/1/items/1', data={'name': new_item_name})
+
+		self.assertTrue('Unauthorised access' in response.content)
+		self.assertEqual(response.status_code, 403)
+
+	def test_successful_delete_bucketlist_items(self):
+		"""
+		Test successful delete attempt on an item in a bucketlist.
+		"""
+		# create the user
+		self.create_user()
+		# login the user
+		self.client.post('/auth/login', data={'username': self.username, 'password': self.password})
+		# create a buckelist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# create an item in the bucketlist
+		item_name = self.fake.name()
+		self.client.post('/bucketlists/1', data={'name': item_name}, headers={'username': token})
+		# delete the item
+		new_item_name = self.fake.name()
+		self.client.post('/bucketlists/1/items/1', data={'name': new_item_name}, headers={'username': token})
+
+		self.assertTrue('Item of id 1 in bucketlist of id 1 has been deleted' in response.content)
+		self.assertEqual(response.status_code, 200)
+
+		# attempt to delete non existent item
+		new_item_name = self.fake.name()
+		self.client.post('/bucketlists/1/items/1', data={'name': new_item_name}, headers={'username': token})
+
+		self.assertTrue('Resource not found' in response.content)
+		self.assertEqual(response.status_code, 404)
+
+	def test_unauthenticated_delete_bucketlist_items(self):
+		"""
+		Test unauthenticated delete attempt on an item in a bucketlist.
+		"""
+		# create the user
+		self.create_user()
+		# login the user
+		self.client.post('/auth/login', data={'username': self.username, 'password': self.password})
+		# create a buckelist
+		bucketlist_name = self.fake.name()
+		self.client.post('/bucketlists/', data={'name': bucketlist_name}, headers={'username': token})
+		# create an item in the bucketlist
+		item_name = self.fake.name()
+		self.client.post('/bucketlists/1', data={'name': item_name}, headers={'username': token})
+		# delete the item
+		new_item_name = self.fake.name()
+		self.client.post('/bucketlists/1/items/1', data={'name': new_item_name})
+
+		self.assertTrue('Unauthorised access' in response.content)
+		self.assertEqual(response.status_code, 403)

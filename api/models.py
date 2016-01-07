0798186c415f2aac4ef5d3a89194517e328bfbfc
Lewis Kabui <lewis.maina@andela.com>
2016-01-07 09:14:51 +0300

    (feature) Add foreign key to items model

--- api/models.py
+++ api/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class BucketList(models.Model):
 	buck_id = models.CharField(max_length=10, primary_key=True)
 	name = models.CharField(max_length=100)
 	date_created = models.DateField(auto_now_add=True)
 	date_modified = models.DateField(auto_now=True)
 	created_by = models.CharField(max_length=100)
+
 
 class Item(models.Model):
 	item_id = models.CharField(max_length=10, primary_key=True)

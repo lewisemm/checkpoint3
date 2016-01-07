cd87ef69732fa60f2a8fc145333484fe728d86fc
Lewis Kabui <lewis.maina@andela.com>
2016-01-07 16:20:36 +0300

    (feature) Create logic to display nested BucketList

--- api/serializers.py
+++ api/serializers.py
@@ -3,13 +3,16 @@ from rest_framework import serializers
 from .models import BucketList, Item
 
 
-class BucketListSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = BucketList
-		fields = ('name',)
-
-
 class ItemSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Item
 		fields = ('bucketlist', 'name', 'done')
+
+
+class BucketListSerializer(serializers.ModelSerializer):
+	item = ItemSerializer(many=True, read_only=True)
+
+	class Meta:
+		model = BucketList
+		fields = ('name', 'item')
+		# write_only_fields = ('item',)

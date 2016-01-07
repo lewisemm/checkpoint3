a01943aa25624ff1b90ac25285fff33b41409836
Lewis Kabui <lewis.maina@andela.com>
2016-01-07 14:14:18 +0300

    (feature) Modify models
    - Create primary field using models.AutoField for auto-increment capability
    - Create constraints for model fields (blank=True)
    - Create migrations

--- api/models.py
+++ api/models.py
@@ -2,17 +2,35 @@ from django.db import models
 
 
 class BucketList(models.Model):
-	buck_id = models.CharField(max_length=10, primary_key=True)
-	name = models.CharField(max_length=100)
-	date_created = models.DateField(auto_now_add=True)
-	date_modified = models.DateField(auto_now=True)
-	created_by = models.CharField(max_length=100)
+	buck_id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=100, blank=False)
+	date_created = models.DateTimeField(auto_now_add=True)
+	date_modified = models.DateTimeField(auto_now=True)
+	created_by = models.CharField(
+		max_length=100,
+		blank=False,
+		default='Unauthenticated'
+	)
+
+	@classmethod
+	def create_bucketlist(cls, current_user, **kwargs):
+		"""Create bucketlist method.
+
+		Enforces the value of the "created_by" field to be the currently logged
+		in user.
+		"""
+
+		bl = BucketList(
+			name=kwargs.get('name'),
+			created_by=current_user.username
+		)
+		bl.save()
 
 
 class Item(models.Model):
-	item_id = models.CharField(max_length=10, primary_key=True)
-	name = models.CharField(max_length=100)
-	date_created = models.DateField(auto_now_add=True)
-	date_modified = models.DateField(auto_now=True)
+	item_id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=100, blank=False)
+	date_created = models.DateTimeField(auto_now_add=True)
+	date_modified = models.DateTimeField(auto_now=True)
 	done = models.BooleanField(default=False)
 	bucketlist = models.ForeignKey(BucketList, on_delete=models.CASCADE)

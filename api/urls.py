9bb19a55cc7a4b2a629cbfd1347a31579a1d5f20
Lewis Kabui <lewis.maina@andela.com>
2016-01-21 11:37:37 +0300

    (chore) Install djangorestframework_jwt and configure settings and url
    - Also updated requirements.txt with newly installed packages

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url, include
 
-from rest_framework_nested import routers
+from rest_framework_nested import routers as nested_router
 
 from .viewsets import BucketListViewSet, ItemViewSet, UserViewSet
 
-router = routers.SimpleRouter()
+router = nested_router.SimpleRouter()
 router.register(r'bucketlists', BucketListViewSet)
 router.register(r'users', UserViewSet)
 
-bucket_router = routers.NestedSimpleRouter(
+bucket_router = nested_router.NestedSimpleRouter(
 	router,
 	r'bucketlists',
 	lookup='bucketlist'
@@ -18,8 +18,9 @@ bucket_router.register(r'items', ItemViewSet, base_name='bucketlist-items')
 urlpatterns = [
 	url(r'^', include(router.urls)),
 	url(r'^', include(bucket_router.urls)),
-	url(r'^api-auth/', include(
-		'rest_framework.urls',
-		namespace='rest_framework')
-	),
+	url(r'^auth/login/', 'rest_framework_jwt.views.obtain_jwt_token'),
+	# url(
+	# 	r'^auth/',
+	# 	include('rest_framework.urls', namespace='rest_framework')
+	# ),
 ]

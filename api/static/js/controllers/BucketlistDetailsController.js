f1b7c4ecd0895fa18939b09873e15e694abe2483
Lewis Kabui <lewis.maina@andela.com>
2016-01-29 11:03:48 +0300

    (feature) Restructure scope bucketlists initialization to apply new BucketlistFactory structure
    - Create scoped methods addItem, editeItem and deleteitem to perform POST, PUT and DELETE operations respectively

--- api/static/js/controllers/BucketlistDetailsController.js
+++ api/static/js/controllers/BucketlistDetailsController.js
@@ -1,17 +1,34 @@
 BucketlistApp.controller('BucketlistDetailsController',
-	['$scope', 'BucketlistFactory', '$routeParams',
-	function ($scope, BucketlistFactory, $routeParams) {
-		var Bucketlist = BucketlistFactory.getOne($routeParams.buckId);
+	['$scope', 'BucketlistFactory', '$routeParams', '$window',
+	function ($scope, BucketlistFactory, $routeParams, $window) {
+		var data = {
+			buck_id: $routeParams.buckId
+		};
+		$scope.bucketlist = BucketlistFactory.Bucketlist.getOne(data);
 
-		Bucketlist.$promise.then(
-			function (response) {
-				$scope.items = response.item;
-				$scope.bucketlist_name = response.name;
-
-			},
-			function (error) {
-				console.log("Fail", error);
+		$scope.addItem = function () {
+			if ($scope.new_item_name) {
+				var data = {
+					buck_id: $routeParams.buckId,
+					name: $scope.new_item_name,
+					done: $scope.new_item_status
+				};
+				BucketlistFactory.Item.create(data);
 			}
-		);
+
+		};
+
+		$scope.editItem = function(buck_id, item_id) {
+			$window.location.href = "#bucketlist/" + buck_id + "/edit/" + item_id + "/";
+		}
+
+		$scope.deleteItem = function (item_id) {
+			var data = {
+				buck_id: $routeParams.buckId,
+				item_id: item_id
+			};
+			BucketlistFactory.ItemDetail.deleteItem(data);
+		}
+
 	}
 ]);

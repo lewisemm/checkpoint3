a49e360a1d975ea35b06424acb457351f9947a0f
Lewis Kabui <lewis.maina@andela.com>
2016-01-30 11:26:06 +0300

    (feature) Refine View. Create validation logic and display error  messages

--- api/static/js/controllers/LoginController.js
+++ api/static/js/controllers/LoginController.js
@@ -3,22 +3,40 @@ BucketlistApp.controller('LoginController',
 	function ($scope, $http, $window, $cookies) {
 
 		$scope.login = function () {
-			var data = {
-				username: $scope.login_username,
-				password: $scope.login_password
-			};
-			$http.post(
-				'http://localhost:8000/auth/login/',
-				data
-			)
-			.then(function (response) {
-				// attach token to authorization header
-				var token = response.data.token;
-				if (token) {
-					$cookies.put('Authorization', token);
-				}
-				$window.location.href = "#bucketlist";
-			});
+			if (!$scope.login_username) {
+				$scope.accessDenied = 'Username missing';
+			}
+			else if (!$scope.login_password) {
+				$scope.accessDenied = 'Password missing';
+			}
+			else {
+				var data = {
+					username: $scope.login_username,
+					password: $scope.login_password
+				};
+				$http.post(
+					'http://localhost:8000/auth/login/',
+					data
+				)
+				.then(
+					function (response) {
+						// attach token to authorization header
+						var token = response.data.token;
+						if (token) {
+							$cookies.put('Authorization', token);
+						}
+						$window.location.href = '#bucketlist';
+					},
+					function (error) {
+						$scope.accessDenied = 'Incorrect username and/or password';
+					}
+				);
+
+			}
 		};
+
+		$scope.removeMsg = function() {
+			$scope.accessDenied = '';
+		}
 	}
 ]);

c162c414795c00e1eaee9d5ad19cf74645d07fb5
Lewis Kabui <lewis.maina@andela.com>
2016-02-08 09:28:07 +0300

    (feature) Replace JS confirm dialog before delete with more visually appealing SweetAlert dialog

--- api/static/js/controllers/BucketlistDetailsController.js
+++ api/static/js/controllers/BucketlistDetailsController.js
@@ -33,13 +33,14 @@ BucketlistApp.controller('BucketlistDetailsController',
 				BucketlistFactory.Item.create(data)
 				.$promise.then(
 					function (response) {
-						$scope.bucketlist = BucketlistFactory.Bucketlist.getOne({buck_id: $routeParams.buckId});
+						$scope.bucketlist = BucketlistFactory.Bucketlist.getOne({buck_id: data.buck_id});
 						var $toastContent = $('<strong style="color: #4db6ac;">Bucketlist item created successfully.</strong>');
 						Materialize.toast($toastContent, 5000);
 						$scope.new_item_name = "";
-						$scope.new_item_status = "";
+						$scope.new_item_status = false;
 					},
 					function (error) {
+						console.log(error);
 						var $toastContent = $('<strong style="color: #f44336;">Error creating bucketlist item.</strong>');
 						Materialize.toast($toastContent, 5000);
 					}
@@ -57,27 +58,37 @@ BucketlistApp.controller('BucketlistDetailsController',
 		}
 
 		$scope.deleteItem = function (item_id) {
-			var conf = confirm('Proceed and delete?');
-
-			if (conf) {
-				var data = {
-					buck_id: $routeParams.buckId,
-					item_id: item_id
-				};
-				BucketlistFactory.ItemDetail.deleteItem(data)
-				.$promise.then(
-					function (response) {
-						var $toastContent = $('<strong style="color: #4db6ac;">Bucketlist item successfully deleted.</strong>');
-						Materialize.toast($toastContent, 5000);
-						$scope.bucketlist = BucketlistFactory.Bucketlist.getOne({buck_id: $routeParams.buckId});
-					}, function (error) {
-						var $toastContent = $('<strong style="color: #f44336;">Bucketlist item not deleted.</strong>');
-						Materialize.toast($toastContent, 5000);
-					}
-				);
-			}
-
+			var data = {
+				buck_id: $routeParams.buckId,
+				item_id: item_id
+			};
+			swal(
+				{
+					title: "Are you sure?",
+					text: "You will not be able to undo this operation!",
+					type: "warning",
+					showCancelButton: true,
+					confirmButtonColor: "#f44336",
+					confirmButtonText: "Yes, delete it!",
+					closeOnConfirm: false
+				},
+				function () {
+					BucketlistFactory.ItemDetail.deleteItem(data)
+					.$promise.then(
+						function (response) {
+							swal("Deleted!", "Item " + data.item_id + " in Bucketlist " + data.buck_id + " has been deleted.", "success");
+							$scope.bucketlist = BucketlistFactory.Bucketlist.getOne({buck_id: data.buck_id});
+						}, function (error) {
+							console.log(error);
+							if (error.status === 403) {
+								sweetAlert("Oops...", "You are not authorized to delete this bucketlist item", "error");
+							} else {
+								sweetAlert("Oops...", error.statusText, "error");
+							}
+						}
+					);
+				}
+			);
 		}
-
 	}
 ]);

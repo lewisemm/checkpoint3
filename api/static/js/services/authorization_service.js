27d63541028fedb34ce45450e701e81e3078b48d
Lewis Kabui <lewis.maina@andela.com>
2016-02-11 12:32:25 +0300

    (fix) Refactor authorization_service to allow posts to public endpoints

--- api/static/js/services/authorization_service.js
+++ api/static/js/services/authorization_service.js
@@ -2,7 +2,13 @@ BucketlistApp.service('AuthorizationService', ['$cookies',
 	function ($cookies) {
 		var service = this;
 		service.request = function (config) {
-			config.headers['Authorization'] = 'JWT ' + $cookies.get('Authorization');
+			token = $cookies.get('Authorization');
+			config.headers = config.headers || {}
+
+			if (token) {
+				config.headers['Authorization'] = 'JWT ' + $cookies.get('Authorization');
+			}
+
 			return config;
 		};
 	}

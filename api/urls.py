bf716172f55731520becb851fdd32a6b8459d26c
Lewis Kabui <lewis.maina@andela.com>
2016-02-11 19:10:14 +0300

    (chore) Create more routes
    - Create route for Django REST framework login
    - Create route for Django REST Swagger documentation package

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,6 @@ from rest_framework_nested import routers as nested_router
 from .viewsets import BucketListViewSet, ItemViewSet, UserViewSet
 from api import views
 
-
-# from api import views
-
 router = nested_router.SimpleRouter()
 router.register(r'bucketlists', BucketListViewSet)
 router.register(r'users', UserViewSet)
@@ -24,4 +21,6 @@ urlpatterns = [
 	url(r'^', include(bucket_router.urls)),
 	url(r'^$', views.index, name='index'),
 	url(r'^auth/login/', 'rest_framework_jwt.views.obtain_jwt_token'),
+	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+	url(r'^api/docs/', include('rest_framework_swagger.urls')),
 ]

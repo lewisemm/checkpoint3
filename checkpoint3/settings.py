b870d975d8c2680a52e11af3a585606303e6cedf
Lewis Kabui <lewis.maina@andela.com>
2016-02-12 16:39:20 +0300

    (chore) Retrieve SECRET_KEY setting from environment variable

--- checkpoint3/settings.py
+++ checkpoint3/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6c_wsu_&boyb=gxo_9om_5ihhvf1#sch4*9e+v*!lwtyhp-kn)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

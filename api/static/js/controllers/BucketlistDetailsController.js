1298352a5d3f31ab2c8bfdf4773d975a84d0cf20
Lewis Kabui <lewis.maina@andela.com>
2016-02-15 09:16:56 +0300

    (feature) Update item status to done when item checkbox is clicked

--- api/static/js/controllers/BucketlistDetailsController.js
+++ api/static/js/controllers/BucketlistDetailsController.js
@@ -1,6 +1,6 @@
 BucketlistApp.controller('BucketlistDetailsController',
-	['$scope', 'BucketlistFactory', '$routeParams', '$window',
-	function ($scope, BucketlistFactory, $routeParams, $window) {
+	['$scope', 'BucketlistFactory', '$routeParams', '$window', '$filter',
+	function ($scope, BucketlistFactory, $routeParams, $window, $filter) {
 		var data = {
 			buck_id: $routeParams.buckId
 		};
@@ -60,7 +60,64 @@ BucketlistApp.controller('BucketlistDetailsController',
 
 		$scope.editItem = function(buck_id, item_id) {
 			$window.location.href = "#bucketlist/" + buck_id + "/edit/" + item_id + "/";
-		}
+		};
+
+		$scope.clickedAsDone = function (item_id) {
+			// retrieve the item object with id of "item_id" from $scope.bucketlist
+			var itemClickedArray = $filter('filter')($scope.bucketlist.item, {item_id: item_id}, function (actual, expected) {
+				return angular.equals(actual, expected);
+			});
+			// only allow updates for items whose done status is false
+			// when the user clicks on the checkbox
+			var itemClicked = itemClickedArray[0];
+
+			if (!itemClicked.done) {
+				//
+				swal(
+					{
+						title: "Proceed to mark item as 'done'?",
+						text: "You are about to mark this item as done!",
+						type: "warning",
+						showCancelButton: true,
+						confirmButtonColor: "#f44336",
+						confirmButtonText: "Yes, update item!",
+						closeOnConfirm: false
+					},
+					function () {
+						var data = {
+							buck_id: $routeParams.buckId,
+							item_id: item_id,
+							done: true,
+							name: itemClicked.name
+						};
+						BucketlistFactory.ItemDetail.edit(data).$promise.then(
+							function (response) {
+								console.log(response);
+								swal("Item status updated!", "The item has now been marked as done!", "success");
+							},
+							function (error) {
+								console.log(error);
+								swal("Oops...!", "Something went wrong when updating the done status", "error");
+							}
+						);
+					}
+				);
+
+			} else {
+				// when the done status was already true, tell the user to press the
+				// edit button where they will be redirected to edit page
+				swal("Item already marked as done!", "Please use the edit button if you still wish to change something.");
+			}
+			BucketlistFactory.Bucketlist.getOne({buck_id: $routeParams.buckId}).$promise.then(
+				function (response) {
+					$scope.bucketlist = response;
+				},
+				function (error) {
+
+				}
+			);
+
+		};
 
 		$scope.deleteItem = function (item_id) {
 			var data = {
@@ -84,16 +141,11 @@ BucketlistApp.controller('BucketlistDetailsController',
 							swal("Deleted!", "Item " + data.item_id + " in Bucketlist " + data.buck_id + " has been deleted.", "success");
 							$scope.bucketlist = BucketlistFactory.Bucketlist.getOne({buck_id: data.buck_id});
 						}, function (error) {
-							console.log(error);
-							if (error.status === 403) {
-								sweetAlert("Oops...", "You are not authorized to delete this bucketlist item", "error");
-							} else {
-								sweetAlert("Oops...", error.statusText, "error");
-							}
+
 						}
 					);
 				}
 			);
-		}
+		};
 	}
 ]);

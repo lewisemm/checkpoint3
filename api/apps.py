973f6f5281f6f6b081454e52a8903f1b68b280b3
Lewis Kabui <lewis.maina@andela.com>
2016-02-25 11:26:15 +0300

    (fix) Resolve QuantifiedCode identified issues

--- api/apps.py
+++ api/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+	"""Configure app name."""
     name = 'api'
--- api/models.py
+++ api/models.py
@@ -24,6 +24,10 @@ class BucketList(models.Model):
 		)
 		bl.save()
 
+	def __str__(self):
+		"""Bucketlist instance representation."""
+		return "<{}>".format(self.name)
+
 
 class Item(models.Model):
 	"""The Item model."""
@@ -37,3 +41,7 @@ class Item(models.Model):
 		on_delete=models.CASCADE,
 		related_name='item'
 	)
+
+	def __str__(self):
+		"""Item instance representation."""
+		return "<{}>".format(self.name)
